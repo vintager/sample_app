2f1a29e2e2e30a6fc6ddc83e0c27ae896292fc49
yxq <vintager4@163.com>
2015-10-21 16:14:17 +0800

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,13 @@ users = User.order(:created_at).take(6)
   content = Faker::Lorem.sentence(5)
   users.each {|user| user.microposts.create!(content: content)}
 end
+
+#following ralationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+#第一个用户关注了第2-50个用户
+following.each { |followed| user.follow(followed) }
+#第3-40号用户关注了第一个用户
+followers.each { |follower| follower.follow(user) }
